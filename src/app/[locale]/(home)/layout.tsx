d7265bc3790c3e013308b0495eedc6cc2b018e79
MaKxex <maksimos651@gmail.com>
2025-10-03 18:02:20 +0300

    cookiebot after body tag

--- src/app/[locale]/(home)/layout.tsx
+++ src/app/[locale]/(home)/layout.tsx
@@ -75,9 +75,9 @@ export default async function RootLayout({ children, params }: { children: React
           <Toaster/>
         </NextIntlClientProvider>
 
-        {/* Cookiebot */}
-        <script id="Cookiebot" src={`https://consent.cookiebot.com/uc.js`} data-cbid={process.env.COOKIEBOT_ID} data-blockingmode="auto" type="text/javascript"></script>
       </body>
+      {/* Cookiebot */}
+      <script id="Cookiebot" src={`https://consent.cookiebot.com/uc.js`} data-cbid={process.env.COOKIEBOT_ID} data-blockingmode="auto" type="text/javascript"></script>
       {/* Google Analytics */}
       <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID || ''} />
     </html>
