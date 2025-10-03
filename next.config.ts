b466724b6302d12bd170b75acd88733d4f7dfe0e
MaKxex <maksimos651@gmail.com>
2025-10-03 19:32:55 +0300

    undo

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const isDev = process.env.NODE_ENV !== 'production';
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' https://challenges.cloudflare.com${isDev ? " 'unsafe-inline' 'unsafe-eval'" : ""};
-  frame-src https://challenges.cloudflare.com;
-  img-src 'self' data: https:;
-  style-src 'self' 'unsafe-inline';
-  connect-src 'self' https://challenges.cloudflare.com https://${process.env.NEXT_PUBLIC_STRAPI_URL?.replace(/^https?:\/\//, "") || "localhost"};
-`;
-
 const nextConfig: NextConfig = {
   experimental: {
     globalNotFound: true,
@@ -29,21 +19,9 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      
     ],
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: ContentSecurityPolicy.replace(/\n/g, '').replace(/\s{2,}/g, ' '),
-          },
-        ],
-      },
-    ];
-  },
+  }
 };
 
 const withNextIntl = createNextIntlPlugin();
