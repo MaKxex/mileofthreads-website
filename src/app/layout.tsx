9c158ad67b43c578ed85861a14ee3eeaada82a20
MaKxex <maksimos651@gmail.com>
2025-11-13 19:51:12 +0200

    redirect mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import { getGlobalMetadata } from "@/lib/metadata";
+import {redirect, RedirectType } from "next/navigation";
+
 
 type PageParams = {
   locale: string;
@@ -10,5 +12,12 @@ export async function generateMetadata({ params }: { params: Promise<PageParams>
 }
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const redirectTo = process.env.NEXT_REDIRECT_TO_;
+  const redirectMode = process.env.NEXT_REDIRECT_MODE === 'true';
+  if (redirectMode && redirectTo) {
+      console.log(123);
+      redirect(redirectTo, RedirectType.replace)
+      
+  }
   return <>{children}</>;
 }
